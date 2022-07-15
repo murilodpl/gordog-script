9396acc1aa5cf9615dc55ed37aa0d4eec9b6bd75
Murilo <inayievmpl@gmail.com>
2022-07-15 09:28:39 -0300

    feat: fix parabens.jsx

--- src/components/Parabens.tsx
+++ src/components/Parabens.tsx
@@ -2,7 +2,7 @@ export default function Parabens() {
     return (
         <div>
             <h1 className="rainbow-letters">Parabéns trouxa</h1>
-            <iframe width="100%" height="500" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&modestbranding&rel=0&loop=1" title="Trouxa" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="500" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&controls=0&modestbranding&rel=0&loop=1" title="Trouxa" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </div>
     )
 }
