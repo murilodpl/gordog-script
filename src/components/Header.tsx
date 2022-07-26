02de0141c1a5a78bb71176f0aa34a15085713e2e
Murilo <inayievmpl@gmail.com>
2022-07-26 17:48:28 -0300

    feat: create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
+import { NavLink } from "react-router-dom";
 import MonkeySound from '../components/MonkeySound';
 
 export default function Header() {
     return (
         <header>
             <MonkeySound />
+
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/" className="navItem" >Converter</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="paises" className="navItem" >Países</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="Info" className="navItem">Info</NavLink>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
