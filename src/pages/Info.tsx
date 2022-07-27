d6fb29d8aaedb8f20247f30da4629f682a505648
Murilo <inayievmpl@gmail.com>
2022-07-27 08:51:36 -0300

    feat: update info page

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -7,7 +7,7 @@ export default function Info() {
             <h2>Sempre confirme se o que foi processado está correto, pode acontecer de haver algum caractere diferente do normal no meio do texto que acaba passando por despercebido pelo código. <span>(Apenas uma olhada por cima basta)</span></h2>
 
             <div className='lista'>
-                <p>Como funciona o script?</p>
+                <p>Como funciona o script de conversão?</p>
                 <ul>
                     <li>Remove todos as quebras de linhas;</li>
                     <li>Remove todos os espaços;
@@ -16,6 +16,14 @@ export default function Info() {
                         </ul>
                     </li>
                     <li>Substitui o caractere <span>- (Hífen)</span> por uma quebra de linha.</li>
+                </ul>
+            </div>
+
+            <div className='lista mt'>
+                <p>Como funciona o script de estados?</p>
+                <ul>
+                    <li>Remove todos as linhas vazias;</li>
+                    <li>Caso você selecione o estado de <span>SP</span>, o script vai manter todas as linhas que possuirem <span>(SP)</span> e remover todo o resto.</li>
                 </ul>
             </div>
         </div>
