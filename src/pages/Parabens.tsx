e894e5e7fd5a29cd78c8179cf3fc234c5b73c6f0
Murilo <inayievmpl@gmail.com>
2022-07-27 08:53:03 -0300

    feat: update parabens video

--- src/pages/Parabens.tsx
+++ src/pages/Parabens.tsx
@@ -1,8 +1,8 @@
 export default function Parabens() {
     return (
-        <div>
+        <div className="container">
             <h1 className="rainbow-letters">Parabéns trouxa</h1>
-            <iframe width="100%" height="500" src="https://www.youtube.com/embed/XomLKTl7UVw?autoplay=1&controls=0&modestbranding&rel=0&loop=1" title="Trouxa" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+            <iframe width="100%" height="500" src="https://www.youtube.com/embed/bPHPRtIa9VM?autoplay=1&controls=0&modestbranding&rel=0&loop=1" title="Trouxa" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
         </div>
     )
 }
