5209df1cfae331080a5f2b22b431e1fc0aebf3a8
Murilo <inayievmpl@gmail.com>
2022-07-29 17:29:59 -0300

    feat: update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,73 @@
-import { NavLink } from "react-router-dom";
+import favIcon from "../assets/img/favicon.svg"
 import MonkeySound from '../components/MonkeySound';
 
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
 export default function Header() {
+    const [mobileMenuShow, setMobileMenuShow] = useState(false)
+
+    function menuBtnClick() {
+        setMobileMenuShow(prevBool => !prevBool)
+    }
+
     return (
         <header>
-            <MonkeySound />
 
             <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/" className="navItem">Converter</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="estados" className="navItem">Estados</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="aspas" className="navItem">Aspas</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="Info" className="navItem">Info</NavLink>
-                    </li>
-                </ul>
+                <div className="container flex flex-wrap justify-between items-center mx-auto">
+                    <div className="flex py-2">
+                        <MonkeySound />
+                    </div>
+
+                    <div className="md:hidden flex items-center">
+                        <button className="outline-none menu-button" aria-label="Open mobile menu" onClick={menuBtnClick}>
+                            <svg className="w-6 h-6 text-gray-500" x-show="! showMenu" fill="none" viewBox="0 00 24 24" stroke="currentColor">
+                                <path d="m4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+                        <ul className="flex flex-col mt-4 md:flex-row md:space-x-6 md:mt-0 md:text-sm">
+                            <li>
+                                <NavLink to="/" className="navItem">Converter</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="estados" className="navItem">Estados</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="aspas" className="navItem">Aspas</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="contador" className="navItem">Contador</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="Info" className="navItem">Info</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className={`${(!mobileMenuShow) && "hidden"} md:hidden border border-g_red rounded`}>
+                    <ul>
+                        <li>
+                            <NavLink to="/" className="navItem" onClick={menuBtnClick}>Converter</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="estados" className="navItem" onClick={menuBtnClick}>Estados</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="aspas" className="navItem" onClick={menuBtnClick}>Aspas</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="contador" className="navItem" onClick={menuBtnClick}>Contador</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="Info" className="navItem" onClick={menuBtnClick}>Info</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     )
--- src/components/MonkeySound.tsx
+++ src/components/MonkeySound.tsx
@@ -1,7 +1,17 @@
-import monkeySvg from '../assets/img/monkey.svg'
+import monkeyFace01 from '../assets/img/monkeyFaces/monkey01.svg'
+import monkeyFace02 from '../assets/img/monkeyFaces/monkey02.svg'
+import monkeyFace03 from '../assets/img/monkeyFaces/monkey03.svg'
+import monkeyFace04 from '../assets/img/monkeyFaces/monkey04.svg'
+import monkeyFace05 from '../assets/img/monkeyFaces/monkey05.svg'
+import monkeyFace06 from '../assets/img/monkeyFaces/monkey06.svg'
+import monkeyFace07 from '../assets/img/monkeyFaces/monkey07.svg'
+import monkeyFace08 from '../assets/img/monkeyFaces/monkey08.svg'
+import monkeyFace09 from '../assets/img/monkeyFaces/monkey09.svg'
 import monkeySound from '../assets/img/msound.mp3'
 
 export default function MonkeySound() {
+    var arrImg = [monkeyFace01, monkeyFace02, monkeyFace03, monkeyFace04, monkeyFace05, monkeyFace06, monkeyFace07, monkeyFace08, monkeyFace09]
+
     function playSound() {
         var audio = document.getElementsByTagName("audio")[0];
         audio.play();
@@ -9,8 +19,8 @@ export default function MonkeySound() {
 
     return (
         <>
-            <button className="btnPageMonkey" type="button" onClick={playSound}>
-                <img width="48px" height="48px" src={monkeySvg} alt="Icone de macaco" />
+            <button type="button" onClick={playSound}>
+                <img width="48px" height="48px" src={arrImg[Math.floor(Math.random() * 9)]} alt="Icone de macaco" />
             </button>
 
             <audio>
