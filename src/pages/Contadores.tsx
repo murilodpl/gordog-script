8dc448445b8f367d4cb7475c26b52044a39729a4
murilodpl <mdpleopoldino@gmail.com>
2023-01-27 14:01:10 -0300

    feat: add react toastify and add toast in contadores page

--- src/pages/Contadores.tsx
+++ src/pages/Contadores.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ContadorLetras from "../components/ContadorLetras";
 import ContadorPalavras from "../components/ContadorPalavras";
 
 export default function Aspas() {
     const [isContadorLetra, setIsContadorLetra] = useState(true);
+
+    useEffect(() => {
+        toast.info('*Se quiser analisar o mesmo texto, copie ele antes de trocar entre letras/palavras e cole-o novamente!', {
+            position: "bottom-right",
+            autoClose: 4000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }, [])
 
     return (
         <div>
@@ -17,6 +32,8 @@ export default function Aspas() {
                 :
                 <ContadorPalavras />
             }
+
+            <ToastContainer className={'text-sm'} />
         </div>
     )
 }
